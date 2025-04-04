64d190a2d70ffad13368124e9eec2d40738912fb
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-04-04 12:02:11 -0300

    create a note

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Note from "./components/Note";
 
 function App() {
   return(
     <>
       <Header />
+      <Note />
       <Footer />
     </>
   )
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+function Note () {
+  const title = "Title of Note";
+  const content = "Content of Note"
+  
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  )
+}
+
+export default Note;
